220b54d477850f3fcbc0077a3758a18abf9bb896
badkoldrysh <ohmygodkir17@gmail.com>
2019-07-22 23:57:54 +0200

    Add new template - add. Modify ArticlesController for work with articles adding

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -4,10 +4,17 @@ namespace App\Controller;
 
 class ArticlesController extends AppController
 {
+    public function initialize()
+    {
+        parent::initialize();
+
+        $this->loadComponent('Paginator');
+        $this->loadComponent('Flash'); // include FlashComponent
+    }
+
     // application/articles/index
     public function index()
-    {
-        $this->loadComponent('Paginator');
+    {        
         // fetch paginate set of articles
         // from the database, using Article Model
         // that is automatically loaded via naming conventions
@@ -16,9 +23,29 @@ class ArticlesController extends AppController
         $this->set(compact('articles'));
     }
 
+    // application/articles/$slug
     public function view($slug = null)
     {
         $article = $this->Articles->findBySlug($slug)->firstOrFail();
         $this->set(compact('article'));
+    }
+
+    public function add()
+    {
+        $article = $this->Articles->newEntity();
+        if ($this->request->is('post')) {
+            $article = $this->Articles->patchEntity($article, $this->request->getData());
+
+            // Hardcoring the user_id is temporary, and will be removed later
+            // when we build authentication out.
+            $article->user_id = 1;
+
+            if ($this->Articles->save($article)) {
+                $this->Flash->success(__('Your article has been saved.'));
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('Unable to add your article.'));
+        }
+        $this->set('article', $article);
     }
 }
