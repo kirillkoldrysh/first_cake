7529a25835f6c4eefcd2045639ee61ed1e48acc6
badkoldrysh <ohmygodkir17@gmail.com>
2019-07-23 00:03:40 +0200

    Modify Model\Table\ArticlesTable: add method for doing something before saving to DB, like slug creating

--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -3,11 +3,25 @@
 namespace App\Model\Table;
 
 use Cake\ORM\Table;
+// the Text class
+use Cake\Utility\Text;
 
 class ArticlesTable extends Table
 {
     public function initialize(array $config)
     {
         $this->addBehavior('Timestamp');
+    }
+
+    public function beforeSave($event, $entity, $options)
+    {
+        // simple variation of slug creating
+        // when we can dublicate new slugs
+        // need to repair it
+        if ($entity->isNew() && !$entity->slug) {
+            $sluggedTitle = Text::slug($entity->title);
+            // trim slug to maximum length defined in schema
+            $entity->slug = substr($sluggedTitle, 0, 191);
+        }
     }
 }
