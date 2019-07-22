64afa0d08c9eff6a1a109af2c6b4719ba2e026af
badkoldrysh <ohmygodkir17@gmail.com>
2019-07-22 21:35:03 +0200

    Add new entity 'Article' for represent DB

---
+++ src/Model/Entity/Article.php
@@ -0,0 +1,18 @@
+<?php
+// src/Model/Entity/Article.php
+namespace App\Model\Entity;
+
+use Cake\ORM\Entity;
+
+// entities represent a single record of our data
+// in database, and provide row level behavior 
+class Article extends Entity
+{
+    // property which controls how properties can be modified
+    // by Mass Assigment
+    protected $_accessible = [
+        '*' => true,
+        'id' => false,
+        'slug' => false
+    ];
+}
