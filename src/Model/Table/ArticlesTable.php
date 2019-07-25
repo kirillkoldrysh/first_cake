e639d96142edbbc247cc738b308de07912d28963
badkoldrysh <ohmygodkir17@gmail.com>
2019-07-25 22:44:28 +0200

    Add searching article by tag feature

--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -3,6 +3,7 @@
 namespace App\Model\Table;
 
 use Cake\ORM\Table;
+use Cake\ORM\Query;
 // the Text class
 use Cake\Utility\Text;
 use Cake\Validation\Validator;
@@ -42,5 +43,30 @@ class ArticlesTable extends Table
             ->minLength('body', 10);
 
         return $validator;
+    }
+
+    public function findTagged(Query $query, array $options) 
+    {
+        $columns = [
+            'Articles.id', 'Articles.user_id', 'Articles.title',
+            'Articles.body', 'Articles.published', 'Articles.created',
+            'Articles.slug',
+        ];
+
+        $query = $query
+            ->select($columns)
+            ->distinct($columns);
+
+        if (empty($options['tags'])) {
+            // If there are no tags provided, find articles that have no tags
+            $query->leftJoinWith('Tags')
+                ->where(['Tags.title IS' => null]);
+        } else {
+            // Find articles that have one or more of the provided tags.
+            $query->innerJoinWith('Tags')
+                ->where(['Tags.title IN' => $options['tags']]);
+        }
+
+        return $query->group(['Articles.id']);
     }
 }
